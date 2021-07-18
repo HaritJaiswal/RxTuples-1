0a3fb4a668ee6b37c550467a3e85e3f93e268c5c
Meghna Jayan <m4mekna@gmail.com>
2021-07-18 13:31:48 +0530

    remove .idea, add main.yml

--- entry/build.gradle
+++ entry/build.gradle
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) pakoito 2015
- * Copyright (C) 2020-21 Application Library Engineering Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
